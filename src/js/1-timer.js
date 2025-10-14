d9e740cf5a810b0544cea02e75de3bb38f8ad1c8
ViktorBond7 <141341341+ViktorBond7@users.noreply.github.com>
2025-10-14 12:37:45 +0200

    timer css

---
+++ src/js/1-timer.js
@@ -0,0 +1,16 @@
+import flatpickr from 'flatpickr';
+const refs = {
+  inputData: document.querySelector('#datetime-picker'),
+};
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
+flatpickr(refs.inputData, {});
