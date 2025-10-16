6ed162f238361416893b3177a52f07566893b902
ViktorBond7 <141341341+ViktorBond7@users.noreply.github.com>
2025-10-16 12:35:59 +0200

    timer js addEventListener

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,30 +1,66 @@
 import flatpickr from 'flatpickr';
+import iziToast from 'izitoast';
+
 const refs = {
   inputData: document.querySelector('#datetime-picker'),
+  btn: document.querySelector('[data-start]'),
   days: document.querySelector('[data-days]'),
   hours: document.querySelector('[data-hours]'),
   minutes: document.querySelector('[data-minutes]'),
-  seconds: document.querySelector('[data-data-seconds]'),
+  seconds: document.querySelector('[data-seconds]'),
 };
 
-console.log(refs);
-
-let selectedDates = null;
+let userSelectedDate;
+refs.btn.disabled = true;
 
 const options = {
   enableTime: true,
-  wrap: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    selectedDates = selectedDates[0];
+    if (selectedDates[0] < new Date()) {
+      iziToast.info({
+        title: 'Hello',
+        message: 'Please choose a date in the future',
+        position: 'center',
+      });
+    } else {
+      userSelectedDate = selectedDates[0];
+      refs.btn.disabled = false;
+    }
   },
 };
 
-refs.inputData.value = options.defaultDate;
+const addLeadingZero = value => {
+  return value.toString().padStart(2, '0');
+};
 
-flatpickr(refs.inputData, { options });
+const dataPickr = flatpickr(refs.inputData, options);
+// flatpickr(refs.inputData, options);
+
+const onBtnClick = e => {
+  e.preventDefault();
+  refs.btn.disabled = true;
+  refs.inputData.disabled = true;
+
+  const intervalId = setInterval(() => {
+    let differenData = userSelectedDate - new Date();
+    if (differenData <= 1000) {
+      clearInterval(intervalId);
+      refs.inputData.disabled = false;
+    }
+
+    const { days, hours, minutes, seconds } = convertMs(differenData);
+
+    refs.days.textContent = addLeadingZero(days);
+    refs.hours.textContent = addLeadingZero(hours);
+    refs.minutes.textContent = addLeadingZero(minutes);
+    refs.seconds.textContent = addLeadingZero(seconds);
+  }, 1000);
+};
+
+refs.btn.addEventListener('click', onBtnClick);
 
 ////////////////////////////////////////////////
 function convertMs(ms) {
